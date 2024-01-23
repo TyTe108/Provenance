993a11982300c4b51aa72516b16ed917e5df91b1
TyTe108 <Tylim@ucsc.edu>
2024-01-22 21:01:42 -0800

    Passed all the tests for articles component

--- components/articles/src/main/java/io/collective/articles/ArticlesController.java
+++ components/articles/src/main/java/io/collective/articles/ArticlesController.java
@@ -7,6 +7,7 @@ import org.eclipse.jetty.server.Request;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArticlesController extends BasicHandler {
     private final ArticleDataGateway gateway;
@@ -19,17 +20,21 @@ public class ArticlesController extends BasicHandler {
     @Override
     public void handle(String target, Request request, HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
         get("/articles", List.of("application/json", "text/html"), request, servletResponse, () -> {
-
-            { // todo - query the articles gateway for *all* articles, map record to infos, and send back a collection of article infos
-
-            }
+            // Query the articles gateway for all articles
+            List<ArticleRecord> allArticles = gateway.findAll();
+            List<ArticleInfo> allArticleInfos = allArticles.stream()
+                    .map(article -> new ArticleInfo(article.getId(), article.getTitle()))
+                    .collect(Collectors.toList());
+            writeJsonBody(servletResponse, allArticleInfos);
         });
 
         get("/available", List.of("application/json"), request, servletResponse, () -> {
-
-            { // todo - query the articles gateway for *available* articles, map records to infos, and send back a collection of article infos
-
-            }
+            // Query the articles gateway for available articles
+            List<ArticleRecord> availableArticles = gateway.findAvailable();
+            List<ArticleInfo> availableArticleInfos = availableArticles.stream()
+                    .map(article -> new ArticleInfo(article.getId(), article.getTitle()))
+                    .collect(Collectors.toList());
+            writeJsonBody(servletResponse, availableArticleInfos);
         });
     }
 }
